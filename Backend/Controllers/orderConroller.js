8858e4a9bbc6c6c4c84775c306c91cbb6d857c40
Uznain Rashid <uznainrashid5@gmail.com>
2025-07-27 11:51:25 -0700

    database model for place-order

---
+++ Backend/Controllers/orderConroller.js
@@ -0,0 +1,34 @@
+
+// API FOR COD
+const placeOrder = async(req,res)=>{
+
+}
+// API for StripeMethod
+const OrderMethodStripe = async(req,res)=>{
+
+}
+// API for RozarPay
+const OrderMethodRozarpay = async(req,res)=>{
+
+}
+// Admin are check The status and update
+const AdminStatus = async(req,res)=>{
+
+}
+// Admin all the order are receive
+const AdminOrder = async (req,res) => {
+    
+}
+
+// All the order are frontend
+const userOrder = async(req,res)=>{
+
+}
+export {
+    placeOrder,
+    OrderMethodRozarpay,
+    OrderMethodStripe,
+    AdminOrder,
+    AdminStatus,
+    userOrder
+}
